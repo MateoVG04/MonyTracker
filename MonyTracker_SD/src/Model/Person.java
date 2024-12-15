b38d6cfe82d0fea682dd0a3f577123b3572bc139
Tobias2512 <hagenaarstobias@gmail.com>
2024-12-15 14:56:18 +0100

    Note toegevoegd.

--- MonyTracker_SD/src/Model/Person.java
+++ MonyTracker_SD/src/Model/Person.java
@@ -15,8 +15,10 @@ public class Person {
     public String getEmail() {
         return email;
     }
-    public void createTicket(String wieBetaald, String wieOntvangt, float prijs, PayBehaviour payBehaviour, Group group) {
-        Ticket ticket = new Ticket(prijs,wieBetaald,wieOntvangt,payBehaviour);
+    // Moet niet hierstaan denk ik, beter in de group klasse. Want een person kan ook een ticket creeëren dat hij niet
+    // zelf heeft betaald, en je kan vanuit hier niet de andere person zien die heeft gepayed.
+    public void createTicket(float totalAmount, Person payer, Group group, PayBehaviour payBehaviour, String tag, String description) {
+        Ticket ticket = new Ticket(totalAmount, payer, group, payBehaviour, tag, description);
         group.addTicket(ticket);
     }
 }
